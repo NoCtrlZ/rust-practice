3f03adfc860796fa1152f9d17d12185e44797082
Hemlocked <phantomofrotten@gmail.com>
2019-05-19 20:41:23 +0900

    htmlを返せる

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
-use actix_web::{server, App, HttpRequest, Responder};
+use actix_web::{fs, server, App, HttpRequest, Responder};
 
 fn hello(req:&HttpRequest)->impl Responder{
     let to =
     req.match_info().get("name").unwrap_or("World");
     format!("Hello{}!",to)
-    }
+}
 
-    fn main(){
-        server::new(||{
-            App::new().resource("/",|r|r.f(hello)).resource("/{name}",|r|r.f(hello))
-            })
-            .bind("localhost:3000")
-            .expect("Cannotbindtoport3000")
-             .run();}
+fn main(){
+    server::new(||{
+        App::new().handler(
+            "/",
+            fs::StaticFiles::new(".").unwrap(),
+        )
+        })
+
+        .bind("localhost:3000")
+        .expect("Can not bind to port 3000")
+        .run();
+}
